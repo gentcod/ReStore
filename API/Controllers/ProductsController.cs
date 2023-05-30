84b93f22176474b5a6fd736534fcceb852292881
Oyefule <drelanorgent@gmail.com>
2023-05-30 22:12:47 +0100

    Data aggregation

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,9 +18,19 @@ public class ProductsController : BaseApiController
    }
 
    [HttpGet]
-   public async Task<ActionResult<List<Product>>> GetProducts()
+   public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
    {
-      return await _context.Products.ToListAsync();
+      var query = _context.Products
+      .Sort(productParams.OrderBy)
+      .Search(productParams.Keyword)
+      .Filter(productParams.Brands, productParams.Types)
+      .AsQueryable();
+
+      var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+      Response.Headers.Add("Pagination", JsonSerializer.Serialize(products.MetaData));
+
+      return products;
    }
 
    [HttpGet("{id}")]
