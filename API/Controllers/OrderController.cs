dec5d5be7208ff298aeda23958dd8b001b5ef88e
Oyefule <drelanorgent@gmail.com>
2023-10-03 23:09:02 +0100

    Order Controller Mod

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,19 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
+using API.Entities;
 using API.Entities.OrderAggregate;
+using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
+   [Authorize]
     public class OrderController : BaseApiController
     {
-      private readonly StoreContext _context;
+        private readonly StoreContext _context;
         public OrderController(StoreContext context)
         {
          _context = context;
@@ -41,6 +40,68 @@ namespace API.Controllers
             .FirstOrDefaultAsync();
 
             return order;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+        {
+            var basket = _context.Baskets.RetrieveBasketWithItems(User.Identity.Name).FirstOrDefault();
+            
+            if (basket == null) return BadRequest(new ProblemDetails{Title = "Could not locate basket"});
+
+            var items = new List<OrderItem>();
+
+            foreach(var item in basket.Items)
+            {
+                var productItem = await _context.Products.FindAsync();
+                var itemOrdered = new ProductItemOrdered 
+                {
+                    ProductId = productItem.Id,
+                    Name = productItem.Name,
+                    PictureUrl = productItem.PictureUrl
+                };
+
+                var orderItem = new OrderItem
+                {
+                    Id = productItem.Id,
+                    ItemOrdered = itemOrdered,
+                    Price = productItem.Price,
+                    Quantity = item.Quantity
+                };
+
+                items.Add(orderItem);
+                productItem.QuantityInStock -= item.Quantity;
+            }
+
+            var subTotal = items.Sum(item => item.Price * item.Quantity);
+            var deliveryFee = subTotal > 100000 ? 0 : 500;
+
+            var order = new Order
+            {
+                OrderItems = items,
+                BuyerId = User.Identity.Name,
+                ShippingAddress = orderDto.ShippingAddress,
+                SubTotal = subTotal,
+                DeliveryFee = deliveryFee
+            };
+
+            _context.Orders.Add(order);
+            _context.Baskets.Remove(basket);
+
+            if (orderDto.SaveAddress)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                user.Address = new UserAddress
+                {
+                    FullName = orderDto.ShippingAddress.FullName,
+                    Address1 = orderDto.ShippingAddress.Address1,
+                    Address2 = orderDto.ShippingAddress.Address2,
+                    City = orderDto.ShippingAddress.City,
+                    State = orderDto.ShippingAddress.State,
+                    Zip = orderDto.ShippingAddress.Zip,
+                    Country = orderDto.ShippingAddress.Country,
+                };
+            }
         }
     }
 }
